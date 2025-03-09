9766df7592d89bd28d56223000adc6476681388b
jkjan <ytom2607@gmail.com>
2025-03-10 05:08:29 +0900

    이미지 오류 시 원본 반환하도록 수정

--- src/image_resizer.py
+++ src/image_resizer.py
@@ -3,19 +3,22 @@ from PIL import Image, ImageOps
 
 
 def resize_image(image_data, width):
-    # Open the image using PIL
-    image = Image.open(io.BytesIO(image_data))
-    image = ImageOps.exif_transpose(image)
+    try:
+        # Open the image using PIL
+        image = Image.open(io.BytesIO(image_data))
+        image = ImageOps.exif_transpose(image)
 
-    if type(width) is int and width > 0:
-        # Resize the image
-        original_width, original_height = image.size
-        new_height = int((width / original_width) * original_height)
-        image = image.resize((width, new_height))
+        if type(width) is int and width > 0:
+            # Resize the image
+            original_width, original_height = image.size
+            new_height = int((width / original_width) * original_height)
+            image = image.resize((width, new_height))
 
-    # Save the resized image to a buffer
-    buffer = io.BytesIO()
-    image.save(buffer, format="webp")
-    buffer.seek(0)
+        # Save the resized image to a buffer
+        buffer = io.BytesIO()
+        image.save(buffer, format="webp")
+        buffer.seek(0)
+        return buffer
 
-    return buffer
+    except IOError:
+        return image_data
